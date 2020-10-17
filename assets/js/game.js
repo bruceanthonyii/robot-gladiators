389b36487774ec5ca2799c9017a374143c48e8a1
Brock <bruceanthonyii@gmail.com>
2020-10-16 23:08:21 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,7 +24,7 @@ var fight = function(enemyName) {
     while(enemyHealth > 0 && playerHealth > 0) {
             // Alert players that they are starting the round
         
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
         
         // if player picks "skip" confirm and then stop the loop 
         if (promptFight === "skip" || promptFight === "SKIP") {
@@ -76,7 +76,7 @@ var fight = function(enemyName) {
 
             // check player's health
             if  (playerHealth <= 0) {
-                window.alert(playerName + " has died")
+                window.alert(playerName + " has died");
                 break;
             } else {
                 window.alert(playerName + " still has " + playerHealth + " health left ");
@@ -92,30 +92,71 @@ var fight = function(enemyName) {
     
     
 };
+// function to start a new game
+var startGame = function() {
+    // reser player stats
+    playerHealth = 110;
+    playerAttack = 10;
+    playerMoney = 10;
 
-for(var i = 0; i < enemyNames.length; ++i) {
+    for(var i = 0; i < enemyNames.length; ++i) {
+        if (playerHealth > 0) {
+            // let player know what round they are in, remember that arrays start at 0 so it needs to have added 1 to it
+            window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+
+            // pick new enemy to fight based on the index of the enemyNames array
+            var pickedEnemyName = enemyNames[i];
+
+            // reset enemyHealth before starting new fight
+            enemyHealth = 50;
+
+            // use debugger to pause script from running and check what's going on at that moment in the code
+            // debugger;
+
+            // pass the pickedEnenmyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            fight(pickedEnemyName);
+        }
+
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }  
+
+    }
+
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
+};
+
+
+// function to end the entire game
+var endGame = function() {
+    // if player is still alive, player wins!
     if (playerHealth > 0) {
-        // let player know what round they are in, remember that arrays start at 0 so it needs to have added 1 to it
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
-
-        // pick new enemy to fight based on the index of the enemyNames array
-        var pickedEnemyName = enemyNames[i];
-
-        // reset enemyHealth before starting new fight
-        enemyHealth = 50;
-
-        // use debugger to pause script from running and check what's going on at that moment in the code
-        // debugger;
-
-        // pass the pickedEnenmyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-        fight(pickedEnemyName);
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     }
-
     else {
-        window.alert("You have lost your robot in battle! Game Over!")
+        window.alert("You've lost your robot in battle.");
     }
+
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+        if (playAgainConfirm) {
+            // restart the game
+            startGame();
+        }
+        else {
+            window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+        }
     
 }
+
+// start tha game when the page loads
+startGame(); 
+
+
+
 
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
